d72cac7ebb10808bd705d2fafdf9361c0a38fbbc
xs3rgii <roales.sergio04@gmail.com>
2025-10-30 12:44:48 +0100

    Actualización automática del sitio

---
+++ _site_jekyll/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+FUENTE_REPO="$(pwd)"
+HTML_REPO="_site_jekyll"
+RAMA="main"
+MENSAJE="Actualización automática del sitio"
+
+bundle exec jekyll build
+
+rsync -av --delete --exclude='.git/' "$FUENTE_REPO/_site/" "$HTML_REPO/"
+
+cd "$HTML_REPO"
+git add .
+git commit -m "$MENSAJE" || echo "No hay cambios que commitear."
+git push origin "$RAMA"
+
+cd "$FUENTE_REPO"
